a6f17360f8f7859308166618fb56322cbe3bb79c
Aleksandr Krotov <a.krotov@openmygame.com>
2024-12-01 13:30:36 +0300

    Initial commit

---
+++ Runtime/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Runtime
+{
+    public class Test : MonoBehaviour
+    {
+        public static GameObject Load()
+        {
+            var result = Resources.Load("Test");
+            return Object.Instantiate(result) as GameObject;
+        }
+
+        private void Start()
+        {
+            Load();
+        }
+    }
+}
