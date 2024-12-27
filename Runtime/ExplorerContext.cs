a78ea9b51968449025189dc43040264681296f85
Aleksandr Krotov <a.krotov@openmygame.com>
2024-12-27 20:01:05 +0300

    Create SafeAreaInitializer

--- Runtime/ExplorerContext.cs
+++ Runtime/ExplorerContext.cs
@@ -1,22 +1,12 @@
 ﻿using PhlegmaticOne.FileExplorer.Configuration;
-using PhlegmaticOne.FileExplorer.ExplorerCore.States;
 using PhlegmaticOne.FileExplorer.Infrastructure.DependencyInjection.Installers;
-using PhlegmaticOne.FileExplorer.Infrastructure.SafeArea.Models;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace PhlegmaticOne.FileExplorer
 {
     internal sealed class ExplorerContext : MonoBehaviour
     {
-        [SerializeField] private CanvasScaler _canvasScaler;
         [SerializeField] private MonoContext _context;
-
-        private void Awake()
-        {
-            var resolution = _canvasScaler.referenceResolution;
-            SafeArea.Initialize(resolution.x, resolution.y);
-        }
 
         public void ConstructAndShow(IExplorerConfig config)
         {
---
+++ Runtime/Infrastructure/SafeArea/SafeAreaInitializer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace PhlegmaticOne.FileExplorer.Infrastructure.SafeArea
+{
+    internal sealed class SafeAreaInitializer : MonoBehaviour
+    {
+        [SerializeField] private CanvasScaler _canvasScaler;
+
+        private void Awake()
+        {
+            var resolution = _canvasScaler.referenceResolution;
+            Models.SafeArea.Initialize(resolution.x, resolution.y);
+        }
+    }
+}
