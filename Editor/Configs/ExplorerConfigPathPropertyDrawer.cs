7269c3043642d6acc342949d0adbdc6b200f857f
Aleksandr Krotov <a.krotov@openmygame.com>
2024-12-28 11:57:37 +0300

    Added "Select startup location" in OS explorer

---
+++ Editor/Configs/ExplorerConfigPathPropertyDrawer.cs
@@ -0,0 +1,45 @@
+﻿using PhlegmaticOne.FileExplorer.Configuration;
+using UnityEditor;
+using UnityEngine;
+using Application = UnityEngine.Device.Application;
+
+namespace Editor.Configs
+{
+    [CustomEditor(typeof(ExplorerConfigScriptable))]
+    internal sealed class ExplorerConfigPathPropertyDrawer : UnityEditor.Editor
+    {
+        private ExplorerConfigScriptable _target;
+        
+        private void OnEnable()
+        {
+            _target = (ExplorerConfigScriptable)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            DrawSelectFolderButtons();
+        }
+
+        private void DrawSelectFolderButtons()
+        {
+            var location = GetStartupLocation();
+            
+            if (GUILayout.Button("Select startup location"))
+            {
+                _target.Value.StartupLocation = EditorUtility.OpenFolderPanel("Select startup folder", location, "");
+            }
+
+            if (GUILayout.Button("Reset location"))
+            {
+                _target.Value.StartupLocation = string.Empty;
+            }
+        }
+
+        private string GetStartupLocation()
+        {
+            var startupLocation = _target.Value.StartupLocation;
+            return string.IsNullOrEmpty(startupLocation) ? Application.persistentDataPath : startupLocation;
+        }
+    }
+}
