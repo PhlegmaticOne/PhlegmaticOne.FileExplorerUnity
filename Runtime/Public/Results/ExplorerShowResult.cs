0597c469a02d4fa9f100bb9d1b14442e3bbcd4d1
Aleksandr Krotov <a.krotov@openmygame.com>
2025-08-05 21:57:26 +0300

    Added TryGetSelection methods

--- Runtime/Public/Results/ExplorerShowResult.cs
+++ Runtime/Public/Results/ExplorerShowResult.cs
@@ -5,6 +5,8 @@ namespace PhlegmaticOne.FileExplorer
 {
     public readonly struct ExplorerShowResult
     {
+        private readonly string[] _selectedFiles;
+
         public static ExplorerShowResult Showed(string[] selectedFiles = null)
         {
             return new ExplorerShowResult(isShowed: true, selectedFiles ?? Array.Empty<string>());
@@ -18,15 +20,21 @@ namespace PhlegmaticOne.FileExplorer
         private ExplorerShowResult(bool isShowed, string[] selectedFiles)
         {
             IsShowed = isShowed;
-            SelectedFiles = selectedFiles;
+            _selectedFiles = selectedFiles;
         }
 
         public bool IsShowed { get; }
-        public string[] SelectedFiles { get; }
 
-        public string GetSingleSelection()
+        public bool TryGetSelection(out string[] selection)
         {
-            return SelectedFiles.FirstOrDefault();
+            selection = _selectedFiles;
+            return _selectedFiles.Length > 0;
+        }
+
+        public bool TryGetSingleSelection(out string selection)
+        {
+            selection = _selectedFiles.FirstOrDefault();
+            return _selectedFiles.Length > 0;
         }
     }
 }
